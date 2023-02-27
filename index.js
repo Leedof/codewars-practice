bbaeec260b5fc7440c0547330be23ac4087b89c3
Leedof <m4kushchenko@gmail.com>
2023-02-27 23:50:45 +0200

    Duplicate Encode

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-'use strict'
+"use strict";
+
+function duplicateEncode(word) {
+  const array = word.toLowerCase().split("");
+  return array
+    .map((current, index, arr) => {
+      const arrCompare = [...arr];
+      arrCompare.splice(index, 1);
+      return arrCompare.includes(current) ? ")" : "(";
+    })
+    .join("");
+}
+
+console.log(duplicateEncode("recede"));
+
+//  Best practice is to
+
+function duplicateEncodeBEST(word) {
+  return word
+    .toLowerCase()
+    .split("")
+    .map(function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+    })
+    .join("");
+}
